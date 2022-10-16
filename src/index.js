cd3417dc29a33c1c51db5063c56b6b17094283bb
joaofp <jvfpdaemoon1@gmail.com>
2022-10-16 17:10:44 -0300

    minimum requisits: done;

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { authRouter } from './routes/auth.routes';
+import { urlsNoAuthRouter, urlsRouter } from './routes/url.routes';
+import { userRouter } from './routes/user.routes';
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,6 +14,11 @@ app.get('/', (req, res, next) => {
         status: 'success'
     });
 });
+
+app.use(authRouter);
+app.use(urlsRouter);
+app.use(urlsNoAuthRouter);
+app.use(userRouter);
 
 app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
