abc0dcc4ca9268c695e3581e8413a6eafd57a35a
joaofp <jvfpdaemoon1@gmail.com>
2022-10-17 08:06:44 -0300

    fixing bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,15 +15,7 @@ async function showProfile(req, res){
             visitCount: 0,
             shortenedUrls: [],
         }
-        url.map(el => {
-            body.visitCount += url.visitCount;
-            body.shortenedUrls.push({
-                id: el.id,
-                shortUrl: el.shortUrl,
-                url: el.url,
-                visitCount: el.visitCount,
-            });
-        })
+        
         return res.status(200).send(body);
     } catch (error) {
         return res.sendStatus(500);
