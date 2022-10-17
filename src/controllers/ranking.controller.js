19c5d10eb84a7210fd56ef5c26b5ed62026ddec5
joaofp <jvfpdaemoon1@gmail.com>
2022-10-17 11:09:58 -0300

    fixing bugs

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -6,6 +6,7 @@ async function showRanking(req, res) {
             'SELECT users.id, users.name, COUNT(urls.id) AS "linksCount", COALESCE(SUM(urls.visitcount), 0) AS visitcount FROM users LEFT JOIN urls ON users.id = urls.userid GROUP BY users.id ORDER BY visitcount DESC, "linksCount" DESC LIMIT 10;'
         );
         return res.status(200).send(ranking.rows);
+        //teste
     } catch (error) {
         return res.sendStatus(500);
     }
