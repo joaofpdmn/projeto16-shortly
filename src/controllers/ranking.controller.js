5aef8116368a345074f8649a41ac88431bee9235
joaofp <jvfpdaemoon1@gmail.com>
2022-10-17 08:41:02 -0300

    fixing bugs

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -3,7 +3,7 @@ import connection from "../database/db.js";
 async function showRanking(req, res) {
     try {
         const ranking = await connection.query(
-            'SELECT users.id, users.name, COUNT(urls.id) AS "linksCount", COALESCE(SUM(urls."visitCount"), 0) AS "visitCount" FROM users LEFT JOIN urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC, "linksCount" DESC LIMIT 10;'
+            'SELECT users.id, users.name, COUNT(urls.id) AS "linksCount", COALESCE(SUM(urls.visitcount), 0) AS visitcount FROM users LEFT JOIN urls ON users.id = urls.userid GROUP BY users.id ORDER BY visitcount DESC, "linksCount" DESC LIMIT 10;'
         ).rows;
         return res.status(200).send(ranking);
     } catch (error) {
