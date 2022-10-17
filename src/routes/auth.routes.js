22c0ca9cea33607aa4f0b13d8fa1ab81c7acebf0
joaofp <jvfpdaemoon1@gmail.com>
2022-10-17 10:51:54 -0300

    fixing bugs

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import { loginAccount, signUpAccount } from '../controllers/auth.controller.js';
+import { showRanking } from '../controllers/ranking.controller.js';
+import { getUrlsById, openShortUrl } from '../controllers/urls.controller.js';
+import { getShortUrlByIdAuth, openShortUrlAuth } from '../middlewares/urls.middleware.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', signUpAccount);
 authRouter.post('/signin', loginAccount);
+authRouter.post('/ranking', showRanking);
+authRouter.get('/urls/:id', getShortUrlByIdAuth, getUrlsById);
+authRouter.get('/urls/open/:shortUrl', openShortUrlAuth, openShortUrl);
 
 export { authRouter };
--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import { showRanking } from '../controllers/ranking.controller.js';
-import { deleteUrl, getUrlsById, openShortUrl, shortenUrl } from '../controllers/urls.controller.js';
+import { deleteUrl, shortenUrl } from '../controllers/urls.controller.js';
 import { userAuth } from '../middlewares/auth.middleware.js';
-import { getShortUrlByIdAuth, openShortUrlAuth } from '../middlewares/urls.middleware.js';
 
 const urlsRouter = express.Router();
 const urlsNoAuthRouter = express.Router();
@@ -10,9 +8,6 @@ const urlsNoAuthRouter = express.Router();
 urlsRouter.use(userAuth);
 
 urlsRouter.post('/urls/shorten', shortenUrl);
-urlsNoAuthRouter.get('/urls/:id', getShortUrlByIdAuth, getUrlsById);
-urlsNoAuthRouter.get('/urls/open/:shortUrl', openShortUrlAuth, openShortUrl);
 urlsRouter.delete('/urls/:id', deleteUrl);
-urlsNoAuthRouter.get('/ranking', showRanking);
 
 export { urlsRouter, urlsNoAuthRouter };
